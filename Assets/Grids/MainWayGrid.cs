5d9359e582bf7adb3d5da713f2cd4e08dadb0e15
Lanceloia <43098606+Lanceloia@users.noreply.github.com>
2024-10-16 01:10:01 +0800

    ReWrite Grids with GameObject

---
+++ Assets/Grids/MainWayGrid.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainWayGrid : Grids
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void onStemCellPassBy()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void onStemCellStay()
+    {
+        throw new System.NotImplementedException();
+    }
+}
