9c4ba871c6e4cabb3925f476477930e8e49b20cc
Lanceloia <43098606+Lanceloia@users.noreply.github.com>
2024-10-17 01:49:04 +0800

    Add Pathogen.

---
+++ Assets/Pathogen/Pathogen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathogen : MonoBehaviour
+{
+    public Position p;
+    //test for github
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("test for github again");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
