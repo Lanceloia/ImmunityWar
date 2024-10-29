2f82eafc16d23a812443cc1aeb93121437e97015
Explorofund <155974541+Explorofund@users.noreply.github.com>
2024-10-29 19:00:26 +0800

    将怪物移动1改为移动1到6的随机值

--- Assets/Board/Board.cs
+++ Assets/Board/Board.cs
@@ -271,7 +271,7 @@ public class Board : MonoBehaviour
             for(int i=0;i< pathogenList.Count; i++)
             {
                 //Debug.Log(i);
-                StartCoroutine(PathogenForward(i, 1/*UnityEngine.Random.Range(1, 7)*/));
+                StartCoroutine(PathogenForward(i, *UnityEngine.Random.Range(1, 7)));
                 //to do :增加交互
             }
 
