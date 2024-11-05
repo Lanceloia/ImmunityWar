92738b5c3a0c4f9268eea81a295157abda68e3f5
Explorofund <155974541+Explorofund@users.noreply.github.com>
2024-11-05 19:29:44 +0800

    地图绑定完成

--- Assets/Grids/MainWayGrid.cs
+++ Assets/Grids/MainWayGrid.cs
@@ -7,6 +7,7 @@ public class MainWayGrid : Grids
     public bool canbuild2x2;                // 当前主路格子能否建造2x2的防御塔
     public bool canbuild2x1;                // 当前主路格子能否建造2x1的防御塔
     public GameObject immuneCellGrid2x2;    // 若能建造，则此处绑定对应的免细胞格子对象
+    public GameObject immuneCellGrid2x1;    // 若能建造，则此处绑定对应的免细胞格子对象
 
     private void _init()
     {
---
+++ Assets/ImmuneCell/BCell.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BCell : ImmuneCell
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rank = 1;
+        attackPower = 3;
+        attackRange = 1;
+        attackSpeed = 1;
+        attackLeft = attackSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Upgrade()
+    {
+        byte temp = attackRange;
+        // 断言此处等级必定小于3（等于3时调用者不该调用此函数）
+        Debug.Assert(rank < 3);
+        rank++;
+        SpriteChange();
+        AttackChange();
+
+        if (rank == 3)
+        {
+            GameObject immune_cell_grid = Board.instance.map.GetComponent<Maps>().GetGridsFromPosition(p);
+            immune_cell_grid.GetComponent<ImmuneCellGrid>().state = ImmuneCellGridState.MaxRank;
+        }
+        
+        if  (temp != attackRange)//如果攻击范围改变，则重新计算攻击目标
+        {
+
+            //Debug.Log("attackRange changed");
+            GridsImmuneChange();
+            
+        }
+    }
+
+    protected override void GridsImmuneChange()
+    {
+        Maps map = Board.instance.map;
+        for (int i = 0; i < map.GridsList.Count; i++)
+        {
+            if (isInAttackRange(map.GridsList[i].GetComponent<Grids>().p))
+            {
+                map.GridsList[i].GetComponent<Grids>().immuneCells.Add(tower);
+            }
+        }
+    }
+
+    private bool isInAttackRange(Position position)
+    {
+        bool isInRange = false;
+        if  ((Mathf.Abs(position.x - p.x) <= attackRange ) && (Mathf.Abs(position.y - p.y) <= attackRange))
+        {
+            isInRange = true;
+        }    
+        else if ((Mathf.Abs(position.x - (p.x+1)) <= attackRange) && (Mathf.Abs(position.y - p.y) <= attackRange))
+        {
+            isInRange = true;
+        }
+        else if ((Mathf.Abs(position.x - p.x) <= attackRange) && (Mathf.Abs(position.y - (p.y+1)) <= attackRange))
+        {
+            isInRange = true;
+        }
+        else if ((Mathf.Abs(position.x - (p.x+1)) <= attackRange) && (Mathf.Abs(position.y - (p.y+1)) <= attackRange))
+        {
+            isInRange = true;
+        }
+        return isInRange;
+    }
+
+    public override void SpriteChange()
+    {
+        //贴图更新
+        if (rank == 2)
+            tower.GetComponent<SpriteRenderer>().color = Color.green;
+        else if (rank == 3)
+            tower.GetComponent<SpriteRenderer>().color = Color.blue;
+    }
+
+
+    public override void AttackChange()
+    {
+        //攻击力更新
+        if (rank == 2)
+        {
+            attackPower = 4;
+            attackSpeed = 2;
+        }
+        else if (rank == 3)
+        {
+            attackPower = 5;
+            attackRange = 2;
+            attackSpeed = 3;
+        }
+    }
+    public override void attack(GameObject pathogen)
+    {
+        if (pathogen == null)
+        {
+            Debug.Log("pathogen is null");
+            return;
+        }
+            
+        while(attackLeft != 0 && pathogen != null)
+        {
+            attackLeft--;
+            pathogen.GetComponent<Pathogen>().onHurt(attackPower);
+        }
+    }
+}
